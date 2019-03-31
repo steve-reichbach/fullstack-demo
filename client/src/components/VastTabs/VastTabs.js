b782ad612b9f9f4e893d9d09d0daa6417ecbb5e7
Steve Reichbach <s.reichbach@sap.com>
2019-03-31 23:10:48 +0300

    table and tabs have been added

---
+++ client/src/components/VastTabs/VastTabs.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+const tabsStyles = {
+    root: {
+        flexGrow: 1,
+    },
+};
+
+class VastTabs extends React.Component {
+    state = {
+        value: 0,
+    };
+
+    handleChange = (event, value) => {
+        this.setState({ value });
+    };
+
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <Paper className={classes.root}>
+                <Tabs
+                    value={this.state.value}
+                    onChange={this.handleChange}
+                    indicatorColor="primary"
+                    textColor="primary"
+                >
+                    <Tab label="Vasts" />
+                    <Tab label="Keywords" />
+                </Tabs>
+            </Paper>
+        );
+    }
+}
+
+VastTabs.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(tabsStyles)(VastTabs);
